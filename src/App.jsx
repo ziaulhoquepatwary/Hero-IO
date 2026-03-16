4f2f3907b16fc3ef71b714dcca5c56cde8941872
Ziaul Haque <ziaulhoque4820@gmail.com>
2026-03-16 23:42:46 +0600

    Project setup done

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+    return (
+        <div>
+            <h1 className='text-4xl text-center font-semibold text-indigo-600'>Start A-5 a new Project HERO.IO</h1>
+        </div>
+    )
+}
+
+export default App
